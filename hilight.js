305ca587693f7d839a2ade90af8c64e4e7234571
Tobbe Lundberg <tobbesweden@gmail.com>
2010-03-04 15:42:42 -0500

    Proper support for html special chars

--- hilight.js
+++ hilight.js
@@ -1,6 +1,6 @@
 function hilight(logText, wordsInclude, wordsHilight) {
-	logText = $('<p/>').text(logText).html();
 	var logLines = logText.split(/\r\n|\r|\n/g);
+	var hashLines = logLines.slice();
 
 	if (wordsInclude && wordsInclude != '') {
 		var wordsToInclude = wordsInclude.split(/\r\n|\r|\n/g);
@@ -28,6 +28,7 @@ function hilight(logText, wordsInclude, wordsHilight) {
 			wordsToHilight.forEach(function(w, index) {
 				var regex = new RegExp(w, "g");
 				var matchArray = null;
+				var hashline = "";
 				var line = "";
 				var ending = "";
 
@@ -35,10 +36,15 @@ function hilight(logText, wordsInclude, wordsHilight) {
 					var foundAt = matchArray.index;
 					var matchLen = matchArray[0].length;
 					line = logLines[i].substring(0, foundAt);
+					hashline = hashLines[i].substring(0, foundAt);
 					var spanInsertion = '<span class="hl' + index % 6 + '">' + matchArray[0] + '</span>';
+					var hashInsertion = '###############' + index % 6 + '##' + matchArray[0] + '#######';
 					line += spanInsertion
+					hashline += hashInsertion
 					line += logLines[i].substring(foundAt + matchLen);
+					hashline += hashLines[i].substring(foundAt + matchLen);
 					logLines[i] = line;
+					hashLines[i] = hashline;
 					regex.lastIndex += spanInsertion.length - matchLen;
 				}
 			});
@@ -47,5 +53,44 @@ function hilight(logText, wordsInclude, wordsHilight) {
 		}
 	}
 
-	return logLines.join('\n');
+	var i = 0;
+	while (i < logLines.length) {
+		var matchArray = null;
+		var regex = /<|>|&/g;
+		var entityLine = "";
+		var hashline = "";
+
+		while ((matchArray = regex.exec(hashLines[i])) != null) {
+			var foundAt = matchArray.index;
+			var matchLen = matchArray[0].length;
+			entityLine = logLines[i].substring(0, foundAt);
+			hashline = hashLines[i].substring(0, foundAt);
+			var entity = htmlEncode(matchArray[0]);
+			entityLine += entity
+			hashline += entity
+			entityLine += logLines[i].substring(foundAt + matchLen);
+			hashline += hashLines[i].substring(foundAt + matchLen);
+			logLines[i] = entityLine;
+			hashLines[i] = hashline;
+			regex.lastIndex += entity.length - matchLen;
+		}
+
+		++i;
+	}
+
+	logText = logLines.join('\n');
+	return logText;
+}
+
+function htmlEncode(htmlChar) {
+	switch (htmlChar) {
+		case '<':
+			return "&lt;";
+		case '>':
+			return "&gt;";
+		case '&':
+			return "&amp;";
+	}
+
+	return htmlChar;
 }
