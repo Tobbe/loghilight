6a343841d3a4dd6c9272a8ec4d1f7aa1383b14a7
Tobbe Lundberg <tobbesweden@gmail.com>
2010-03-27 10:45:49 -0400

    Re-implemented the highlighting as a highlighter class. Also fixed a bug with overlapping matches.

--- hilight.js
+++ hilight.js
@@ -1,78 +1,129 @@
-function hilight(logText, wordsHilight) {
-	var logLines = logText.split(/\r\n|\r|\n/);
-	var hashLines = logLines.slice();
+function Highlighter() {
+	var that = this;
 
-	if (wordsHilight && wordsHilight != '') {
-		var wordsToHilight = wordsHilight.split(/\r\n|\r|\n/);
+	this.highlight = function (logText, wordsToHighlight) {
+		hitlist = that.findMatches(logText, wordsToHighlight);
+		hitlist.sort(function(lhs, rhs) {
+			if (lhs.index < rhs.index) {
+				return -1;
+			} else if (lhs.index > rhs.index) {
+				return 1;
+			}
 
-		var i = 0;
-		while (i < logLines.length) {
-			wordsToHilight.forEach(function(w, index) {
-				var regex = new RegExp(w, "g");
-				var matchArray = null;
-				var hashline = "";
-				var line = "";
-				var ending = "";
+			// lhs.index == rhs.index
+			if (lhs.text[1] == '/' && rhs.text[1] != '/') {
+				return -1;
+			} else if (lhs.text[1] != '/' && rhs.text[1] == '/') {
+				return 1;
+			}
 
-				while (w != '' && (matchArray = regex.exec(logLines[i])) != null) {
-					var foundAt = matchArray.index;
-					var matchLen = matchArray[0].length;
-					line = logLines[i].substring(0, foundAt);
-					hashline = hashLines[i].substring(0, foundAt);
-					var spanInsertion = '<span class="hl' + index % 6 + '">' + matchArray[0] + '</span>';
-					var hashInsertion = '###############' + index % 6 + '##' + matchArray[0] + '#######';
-					line += spanInsertion
-					hashline += hashInsertion
-					line += logLines[i].substring(foundAt + matchLen);
-					hashline += hashLines[i].substring(foundAt + matchLen);
-					logLines[i] = line;
-					hashLines[i] = hashline;
-					regex.lastIndex += spanInsertion.length - matchLen;
-				}
-			});
+			return 0;
+		});
 
-			++i;
-		}
-	}
+		var output = "";
 
-	var i = 0;
-	while (i < logLines.length) {
-		var matchArray = null;
-		var regex = /<|>|&/g;
-		var entityLine = "";
-		var hashline = "";
+		var previousHit = new Hit(0, 0, '', '');
+		hitlist.forEach(function(hit, index) {
+			output += that.htmlEncode(logText.substring(previousHit.index, hit.index)) + hit.text;
+			previousHit = hit;
+		});
 
-		while ((matchArray = regex.exec(hashLines[i])) != null) {
-			var foundAt = matchArray.index;
-			var matchLen = matchArray[0].length;
-			entityLine = logLines[i].substring(0, foundAt);
-			hashline = hashLines[i].substring(0, foundAt);
-			var entity = htmlEncode(matchArray[0]);
-			entityLine += entity
-			hashline += entity
-			entityLine += logLines[i].substring(foundAt + matchLen);
-			hashline += hashLines[i].substring(foundAt + matchLen);
-			logLines[i] = entityLine;
-			hashLines[i] = hashline;
-			regex.lastIndex += entity.length - matchLen;
+		var t = logText.substring(previousHit.index)
+		output += this.htmlEncode(t);
+
+		return output;
+	};
+
+	this.findMatches = function (logText, wordsToHighlight) {
+		if (logText == null || logText == "") {
+			return [];
 		}
 
-		++i;
-	}
+		var logLines = logText.split(/\r\n|\r|\n/);
+		var hits = [];
+		var newlineCharCount = 1; // At least one
+		if (logText != "" && logLines[0] != "" && logText[logLines[0].length + 1] == '\n' || logText[logLines[0].length + 1] == '\r') {
+			newlineCharCount = 2;
+		}
 
-	logText = logLines.join('\n');
-	return logText;
+		if (wordsToHighlight && wordsToHighlight != '') {
+			var wordsToHilight = wordsToHighlight.split(/\r\n|\r|\n/).filter(function(line) { return line != ""; });
+
+			var i = 0;
+			var accumulatedLength = 0;
+			while (i < logLines.length) {
+				wordsToHilight.forEach(function(w, index) {
+					var regex = new RegExp(w, "g");
+					var matchArray = null;
+					var line = "";
+
+					while ((matchArray = regex.exec(logLines[i])) != null) {
+						var foundAt = matchArray.index;
+						var matchLen = matchArray[0].length;
+
+						hits.push(new Hit(i, accumulatedLength + foundAt, matchArray[0], '<span class="hl' + index % 6 + '">'));
+						hits.push(new Hit(i, accumulatedLength + foundAt + matchLen, matchArray[0], '</span>'));
+
+						line = logLines[i].substring(0, foundAt);
+					}
+				});
+
+				accumulatedLength += logLines[i].length + newlineCharCount;
+
+				++i;
+			}
+		}
+
+		return hits;
+	};
+
+	this.htmlEncode = function(htmlString) {
+		var encoded = "";
+		for (var i = 0; i < htmlString.length; ++i) {
+			encoded += this.htmlEncodeChar(htmlString[i]);
+		}
+
+		return encoded;
+	};
+
+	this.htmlEncodeChar = function(htmlChar) {
+		switch (htmlChar) {
+			case '<':
+				return "&lt;";
+			case '>':
+				return "&gt;";
+			case '&':
+				return "&amp;";
+		}
+
+		return htmlChar;
+	};
 }
 
-function htmlEncode(htmlChar) {
-	switch (htmlChar) {
-		case '<':
-			return "&lt;";
-		case '>':
-			return "&gt;";
-		case '&':
-			return "&amp;";
-	}
-
-	return htmlChar;
+function Hit(row, index, match, text) {
+	this.row = row;
+	this.index = index;
+	this.match = match;
+	this.text = text;
 }
+
+//TODO: I'll probably need something like this:
+/*
+var	regex = /^/gm,
+	subject = "A\nB\nC",
+	match,
+	endPositions = [];
+
+while (match = regex.exec(subject)) {
+	var zeroLengthMatch = !match[0].length;
+	// Fix IE's incorrect lastIndex
+	if (zeroLengthMatch && regex.lastIndex > match.index)
+		regex.lastIndex--;
+
+	endPositions.push(regex.lastIndex);
+
+	// Avoid an infinite loop with zero-length matches
+	if (zeroLengthMatch)
+		regex.lastIndex++;
+}
+*/
